b82859c6fd87ea9c076660d7a343d1dbf51dff12
theblackmamba08 <fossocabrel08@gmail.com>
2025-09-03 19:40:37 +0100

    Deploying full app

--- Controllers/notes.js
+++ Controllers/notes.js
@@ -1,60 +1,68 @@
-const notesRouter = require('express').Router();
-const Note = require('../models/note');
+const notesRouter = require('express').Router()
+const Note = require('../models/note')
 
 // GET toutes les notes
-notesRouter.get('/', (request, response) => {
-    Note.find({}).then(notes => response.json(notes))
-});
+notesRouter.get('/', async (request, response) => {
+    const notes = await Note.find({})
+    response.json(notes)
+})
 
 // GET note par ID
-notesRouter.get('/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) response.json(note)
-            else response.status(404).end()
-        })
-        .catch(error => next(error))
-});
+notesRouter.get('/:id', async (request, response) => {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
 
 // POST nouvelle note
-notesRouter.post('/', (request, response, next) => {
-    const { content, important } = request.body;
-    const note = new Note({ content, important});
-    note.save()
-        .then(savedNote => response.json(savedNote))
-        .catch(error => next(error))
-});
+notesRouter.post('/', async (request, response) => {
+    const { content, important } = request.body
+    const note = new Note({ content, important })
+
+    const savedNote = await note.save()
+    response.status(201).json(savedNote)
+})
 
 // PUT mise à jour note
 notesRouter.put('/:id', (request, response, next) => {
-    const { content, important } = request.body;
+    const { content, important } = request.body
     Note.findById(request.params.id)
         .then(note => {
             if (!note) {
                 return response.status(410).json({
-                    message: "La note a déjà été supprimée du serveur."
-                });
+                    message: 'La note a déjà été supprimée du serveur.'
+                })
             }
-            note.content = content;
-            note.important = important;
+            note.content = content
+            note.important = important
             return note.save().then(updatedNote => response.json(updatedNote))
         })
         .catch(error => next(error))
-});
+})
 
 // DELETE note
-notesRouter.delete('/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (!note) {
-                return response.status(410).json({
-                    message: "La note a déjà été supprimée du serveur."
-                });
-            }
-            return Note.findByIdAndDelete(request.params.id)
-                .then(() => response.status(204).end());
-        })
-        .catch(error => next(error))
-});
+// notesRouter.delete('/:id', (request, response, next) => {
+//     Note.findById(request.params.id)
+//         .then(note => {
+//             if (!note) {
+//                 return response.status(410).json({
+//                     message: 'La note a déjà été supprimée du serveur.'
+//                 })
+//             }
+//             return Note.findByIdAndDelete(request.params.id)
+//                 .then(() => response.status(204).end())
+//         })
+//         .catch(error => next(error))
+// })
 
-module.exports = notesRouter;
+notesRouter.delete('/:id', async (request, response) => {
+    await Note.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+})
+
+
+module.exports = notesRouter
--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const config = require('./utils/config');
-const logger = require('./utils/logger');
-const middleware = require('./utils/middleware');
-const path = require('path');
-const notesRouter = require('./Controllers/notes');
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const path = require('path')
+const notesRouter = require('./controllers/notes')
 
-const app = express();
+const app = express()
 
-logger.info('Connecting to', config.MONGODB_URI);
+logger.info('Connecting to', config.MONGODB_URI)
 
 mongoose
     .connect(config.MONGODB_URI)
     .then(() => {
-        logger.info('Connected to MongoDB');
+        logger.info('Connected to MongoDB')
     })
     .catch((error) => {
-        logger.error('Error connecting to MongoDB:', error.message);
-    });
+        logger.error('Error connecting to MongoDB:', error.message)
+    })
 
-app.use(express.static('dist'));
-app.use(express.json());
-app.use(middleware.requestLogger);
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.get('/', (request, response) => {
-    response.sendFile(path.resolve(__dirname, 'dist', 'index.html')); 
-});
+    response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
 
-app.use('/api/notes', notesRouter);
+app.use('/api/notes', notesRouter)
 
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-module.exports = app;
+module.exports = app
